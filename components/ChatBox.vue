0bc38ebe6f703979c61d3d01d7ce4112f4e65a9e
Zachary Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-05-03 10:07:29 -0500

    Add basic chat bubble functionality

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -26,6 +26,30 @@ watch(open, () => {
     (input.value as HTMLInputElement).focus();
   });
 });
+
+function getUser(id: string) {
+  return props.users.find((user) => user.id === id);
+}
 </script>
 <template>
+  <div class="fixed bottom-[10px] right-[10px]">
+    <button v-show="!open" @click="open = true" class="bg-blue-500 p-3 rounded">
+      <IconChat class="h-8 w-8 text-white" />
+    </button>
+    <div
+      v-if="open"
+      class="box bg-gray-300 dark:bg-gray-800 rounded w-[450px] overflow-hidden"
+    >
+      <!-- Messages -->
+      <div class="messages p-4 overflow-y-scroll max-h-[80vh]">
+        <ChatBubble
+          v-for="message in messages"
+          :key="message.id"
+          :message="message"
+          :user="getUser(message.userId)"
+          :my-message="message.userId === me.id"
+        />
+      </div>
+    </div>
+  </div>
 </template>
