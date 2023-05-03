4c958575aafe37145756f685a86f7593932141f2
Zachary Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-05-03 09:37:51 -0500

    Add initial imports and properties

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import { nanoid } from "nanoid";
+import { Message, User } from "~~/types";
+const props = withDefaults(
+  defineProps<{
+    messages: Message[];
+    users: User[];
+    me: User;
+    usersTyping?: User[];
+  }>(),
+  {
+    usersTyping: () => [],
+  }
+);
+</script>
 <template>
-  <!-- Code out the chatbox for Exercise 1 here -->
 </template>
