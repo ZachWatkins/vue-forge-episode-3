969c750790167885feeecf3314ff0788db499c82
Zachary Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-05-03 10:19:42 -0500

    Add default message

--- components/ChatBox.vue
+++ components/ChatBox.vue
@@ -63,7 +63,16 @@ function getUser(id: string) {
         </button>
       </header>
       <!-- Messages -->
-      <div class="messages p-4 overflow-y-scroll max-h-[80vh]">
+        <div v-if="!messages.length" class="text-center w-[350px] m-auto">
+          <strong class="text-lg">Chat with Botman!</strong>
+          <p>Our A.I. powered assistant</p>
+          <strong class="block mt-10">Go ahead and ask us something:</strong>
+          <ul class="list-inside list-disc text-left">
+            <li>What is social media post generator?</li>
+            <li>How can I get human support?</li>
+            <li>How was this tool built?</li>
+          </ul>
+        </div>
         <ChatBubble
           v-for="message in messages"
           :key="message.id"
